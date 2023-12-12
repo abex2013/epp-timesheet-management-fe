e8724c63e6bf434b41aa7687658784f756810dec
Abebayehu Tefera <abebayehutefera@gmail.com>
2023-12-12 18:00:29 +0300

    Initial commit

---
+++ apps/project-management/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ProjectService } from './core';
+
+@Component({
+  selector: 'exec-epp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent   {
+
+ title = 'Project Management';
+   
+
+}
